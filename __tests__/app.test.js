fec260de96d21fa117dea148da2ea40a04f94057
Jake Thrasher <mjakethrasher@gmail.com>
2021-04-06 10:44:50 -0700

    fix test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,24 +16,24 @@ describe('back-end-s3-storage routes', () => {
     return setup(pool);
   });
 
-
-const testBassist = {
-  image:'larry-graham.jpg',
-  fullName:'Larry Graham',
-  associatedActs:'Sly and The Family Stone, Graham Central Station'
-}
-
-it('creates a bassist profile', async()=>{
-  
-  const data = await request(app)
-  .post('/api/v1/upload')
-  .send(testBassist)
-  
-  expect(data.body).toEqual({
-    id:expect.any(String),
-    image:expect.any(String),
+  const testBassist = {
+    image:'larry-graham.jpg',
     fullName:'Larry Graham',
     associatedActs:'Sly and The Family Stone, Graham Central Station'
+  }
+  
+  it('creates a bassist profile', async()=>{
+    
+    const data = await request(app)
+    .post('/api/v1/upload')
+    .send(testBassist)
+    
+    expect(data.body).toEqual({
+      id:expect.any(String),
+      image:expect.any(String),
+      fullName:'Larry Graham',
+      associatedActs:'Sly and The Family Stone, Graham Central Station'
+    })
   })
-})
-});
+  });
+
